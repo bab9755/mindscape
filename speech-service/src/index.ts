7a08ca0afe4d8107aacba07d5799211dd4b60c0c
Boubacar Ballo <bab9755@nyu.edu>
2025-07-10 23:51:06 +0400

    add main server

--- speech-service/src/index.ts
+++ speech-service/src/index.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
+import speechRouter from './routes/speech';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/speech', speechRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from the speech to text server!');
