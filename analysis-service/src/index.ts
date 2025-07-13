ef7f5981f70a78e2eb53af32a77f52ea8cdae7f4
Boubacar Ballo <bab9755@nyu.edu>
2025-07-13 21:18:04 +0400

    finish server setup

--- analysis-service/src/index.ts
+++ analysis-service/src/index.ts
@@ -1,13 +1,16 @@
-    // src/server.ts
-    import express, { Request, Response } from 'express';
+import express, { Request, Response } from 'express';
+import analysisRouter from './routes/analysis-agent';
 
-    const app = express();
-    const port = process.env.PORT || 3001;
+const app = express();
+const port = process.env.PORT || 3001;
 
-    app.get('/', (req: Request, res: Response) => {
-      res.send('Hello from TypeScript server!');
-    });
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello from TypeScript server!');
+});
 
-    app.listen(port, () => {
-      console.log(`Server running at http://localhost:${port}`);
-    });
+app.use(express.json());
+app.use('/analyser-agent', analysisRouter);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
