09f8a67db1fe27b95798a9a44a13fa5b74eb93bd
Boubacar Ballo <bab9755@nyu.edu>
2025-07-23 21:52:18 +0400

    update supase keys

--- frontend/lib/supabase.ts
+++ frontend/lib/supabase.ts
@@ -4,8 +4,10 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient, processLock } from '@supabase/supabase-js'
 
 
-const supabaseUrl = process.env.SUPABASE_URL || ''
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || ''
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || ''
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || ''
+
+console.log(supabaseUrl, supabaseAnonKey)
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
